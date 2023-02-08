1db4281d53d6cda870956fb004e0b6a8d8439cce
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-02-08 10:15:28 +0100

    mise au propre

--- front/js/product.js
+++ front/js/product.js
@@ -1,29 +1,29 @@
-// récupération de l'id du produit pour l'écrire dans la page produit 
+// récupération de l'id du produit pour l'écrire dans la page produit
+
 const str = window.location.href;
 const url = new URL(str);
 const currentId = url.searchParams.get("id");
 
-// création variable pour page api ensemble produits, et une autre pour la page d'un produit 
-const apiProductsUrl = 'http://localhost:3000/api/products/';
+// création variable pour page api ensemble produits, et une autre pour la page d'un produit
+
+const apiProductsUrl = "http://localhost:3000/api/products/";
 const productPage = apiProductsUrl + currentId;
 
-/*  - écrire variable pour cibler l'élément dans le HTML et pour définir les autres besoins
-    - envoyer requête avec fech pour avoir les données désirées
-    - si la réponse est ok, on transforme la réponse en format json
-    - les valeurs que l'on a, nous allons les "entrer" dans le DOM
-    - si il y a une erreur, la console nous affichera le message d'erreur : "Une erreur est survenue
-    + description de l'erreur"  */
+// envoyer requête avec fech pour avoir les données désirées
+// si la réponse est ok, on transforme la réponse en format json
 
 fetch(productPage)
-    .then(function(res) {
+    .then(function (res) {
         if (res.ok) {
-            return res.json();
+        return res.json();
     }
-})
-    .then(function(data) {
+    })
 
+// les valeurs que l'on a, nous allons les "entrer" dans le DOM
+
+    .then(function (data) {
         const productImage = document.querySelector(".item__img");
-        productImage.innerHTML = `<img src= ${data.imageUrl} alt= "${data.altTxt}">`; 
+        productImage.innerHTML = `<img src= ${data.imageUrl} alt= "${data.altTxt}">`;
 
         const productName = document.getElementById("title");
         productName.innerText = `${data.name}`;
@@ -33,91 +33,101 @@ fetch(productPage)
 
         const productDescription = document.getElementById("description");
         productDescription.innerText = `${data.description}`;
-        
-        let colors = data.colors;    
+
+        let colors = data.colors;
         colors.forEach((element) => {
             let color = document.getElementById("colors");
             color.innerHTML += `<option value="${element}">${element}</option>`;
-        })
-    })
+        });
+    });
 
-/*  logique fonctionnelle
-    1) en cliquant sur le bouton    (on cible l'élément dans le html | on écoute l'évènement)  
-    2) je veux récupérer la couleur choisie, le nombre d'article, l'id produit  (on cible l'élément dans le html | on récupère la valeur)
-    3) s'assurrer que l'utilisateur a bien choisi une couleur
-    4) si l'utilisateur a un nbre <1 , afficher un message lui disant +1    (condition si la quantité est <1 -> message d'alerte)
-    5) j'aimerai les stocker    */
+                                                /////////   logique fonctionnelle   /////////
+
+// en cliquant sur le bouton (on cible l'élément dans le html | on écoute l'évènement)
 
 let productAddToCart = document.getElementById("addToCart");
-productAddToCart.addEventListener("click", function() {
-    //console.log(currentId)
+productAddToCart.addEventListener("click", function () {
 
-let productAddColor = document.getElementById("colors")
-    //console.log(colors.value);
-    if (colors.value = colors.value) {}
-    else {    
-            alert("veuillez choisir une couleur");
+// je récupère la couleur choisie, la quantité  (on cible l'élément dans le html | on récupère la valeur)
+// messages d'alertes si couleur non choisie et si quantité <1
+
+    let productAddColor = document.getElementById("colors");
+        console.log(colors.value);
+    if ((colors.value = colors.value)) {
+    } else {
+    alert("veuillez choisir une couleur");
     }
 
-let productAddQuantity = document.getElementById("quantity")
-    //console.log(quantity.value);
-    if (quantity.value < 1 ) {
-        alert("veuillez choisir une quantité")
+    let productAddQuantity = document.getElementById("quantity");
+        console.log(quantity.value);
+    if (quantity.value < 1) {
+    alert("veuillez choisir une quantité");
     }
 
-let product = {id:currentId, quantity:quantity.value, colors:colors.value}
-    //console.log(product)
+    let product = {
+        id: currentId,
+        quantity: quantity.value,
+        colors: colors.value,
+    };
 
-/*  1) recupérer le panier (panier)
-    2) créer le panier (arrayCart : tableau vide pour éviter d'effacer à chaque fois le produit)
-    3) ajouter produit au tableau
-    4) panier à mettre dans le local storage    */
+// récupérer le panier existant
 
-let cart = window.localStorage.getItem("cart");
+    let cart = window.localStorage.getItem("cart");
     cart = JSON.parse(cart);
         console.log(cart);
-        
-    if (cart === null) { 
-            console.log("Cart is empty!");
+
+// si pas de panier -> créer le panier (arrayCart : tableau vide pour éviter d'effacer à chaque fois le produit)
+// ajouter produit au tableau / panier à mettre dans le local storage
+
+    if (cart === null) {
+        console.log("Cart is empty!");
         cart = [];
         firstAdd();
             console.log(cart);
-        return;      
+        return;
     }
 
-let search_in_cart = cart.find(element => element.id === product.id && element.colors === product.colors);
-    console.log(search_in_cart);
-    
-/* fonction remplacer objet existant
-    retirer objet meme id et color
-    ajoute produit (qté déjà modifié) 
-    faire autre fct 
-*/
+// rechercher si objet (même id et couleur) déjà dans le panier
+
+    let search_in_cart = cart.find(
+    (element) => element.id === product.id && element.colors === product.colors);
+        console.log(search_in_cart);
+
+// si objet différent -> fonction firstAdd
+
     if (!search_in_cart) {
-            console.log(!search_in_cart);
+        console.log(!search_in_cart);
         firstAdd();
             console.log(cart);
 
+// si objet idem (id + color) -> rechercher l'index / on déclare les valeurs de l'objet
+// id + color + addition quantité objet à celle du panier
+
     } else if (search_in_cart) {
-            console.log(cart);
-        const indexOfSameProductInCart = cart.indexOf (search_in_cart);
+        console.log(cart);
+        const indexOfSameProductInCart = cart.indexOf(search_in_cart);
             console.log(indexOfSameProductInCart);
-        
-            if (indexOfSameProductInCart !==-1) {
-                cart [indexOfSameProductInCart] = {id: currentId, quantity: parseInt(quantity.value) + parseInt(search_in_cart.quantity), colors: colors.value}
-                let jsonCart = JSON.stringify(cart);
-                let setCart = window.localStorage.setItem("cart", jsonCart);
-            }
-            console.log (search_in_cart.quantity);
-            console.log (product.quantity);
-            console.log (cart);
-        
-    } 
-    
-function firstAdd () {
-    cart.push(product);
-        //console.log(cart);
+
+    if (indexOfSameProductInCart !== -1) {
+        cart[indexOfSameProductInCart] = {
+            id: currentId,
+            quantity: parseInt(quantity.value) + parseInt(search_in_cart.quantity),
+            colors: colors.value,
+        };
     let jsonCart = JSON.stringify(cart);
     let setCart = window.localStorage.setItem("cart", jsonCart);
     }
-})
+    console.log(search_in_cart.quantity);
+    console.log(product.quantity);
+    console.log(cart);
+    }
+
+// déclaration fonction -> ajout valeurs objet dans le panier / transformation format Json / envoyer au local storage
+
+    function firstAdd() {
+    cart.push(product);
+        // console.log(cart);
+    let jsonCart = JSON.stringify(cart);
+    let setCart = window.localStorage.setItem("cart", jsonCart);
+    }
+});
