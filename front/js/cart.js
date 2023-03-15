758fcaed2a1c3eca87147aabce6b1f9ef3cd1024
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-03-15 19:44:18 +0100

    Màj évènement change

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,9 +6,7 @@ function getCart() {
     cart = JSON.parse(cart);
     console.log(cart);
 }
-
 getCart();
-// console.log(cart);
 
 let data;
 cart.forEach((element) => {
@@ -24,7 +22,6 @@ cart.forEach((element) => {
             alert("Un problème est survenu");
         } else {
             data = await data.json();    // on attend que la requete soit finie et transforme le json en objet js
-            // console.log(data);
 
             const addInfoProduct = document.getElementById("cart__items");
             addInfoProduct.innerHTML += `
@@ -51,9 +48,8 @@ cart.forEach((element) => {
 </article>`;
             //* appels des fonctions
             totalCartQuantity();
-
             totalCartPrice(data.price * element.quantity);
-            // deleteProduct();
+            deleteProduct();
         }
     }
     collectData();
@@ -73,7 +69,7 @@ function totalCartQuantity() {
     }
 }
 
-//! montant total du panier ---> 
+//! montant total du panier ---> ok
 let cartPrice = 0;
 function totalCartPrice(price) {
     cartPrice += price;
@@ -82,30 +78,41 @@ function totalCartPrice(price) {
     addTotalCartPrice.innerHTML = cartPrice;
 }
 
-// //! supprimer un objet dans le panier et le local storage
+//! supprimer un objet dans le panier et le local storage ---> supprime un objet fc° puis NOK
+function deleteProduct() {
+    let canapes = document.getElementsByClassName("deleteItem");
 
-// function deleteProduct() {
-//     let canapes = document.getElementsByClassName("deleteItem");       // on cible l'élément pour supprimer dans le dom
-//     // console.log(canapes);
+    for (let deleteCanape of canapes) {
+        deleteCanape.addEventListener("click", function () {
+            let searchArticle = deleteCanape.closest("article");
+            // console.log(searchArticle);
+            let searchId = searchArticle.dataset.id;
+            // console.log(searchId);
+            let searchColor = searchArticle.dataset.color;
+            // console.log(searchColor);
+            let elToKeep = cart.filter(el => el.id !== searchId || el.colors !== searchColor);
+            // console.log(elToKeep);
+            cartUpdateDelete(elToKeep);
+            searchArticle.remove();
 
-//     for (let deleteCanape of canapes) {
-//         canapes.addEventListener("click", function () {           // on écoute l'évènement click
+        })
+        function cartUpdateDelete(x) {   // màj du panier dans le local storage
+            localStorage.clear();
+            let cart = [];
+            cart.push(x);
+            console.log(cart);
+            let jsonCart = JSON.stringify(cart);
+            let setCart = window.localStorage.setItem("cart", jsonCart);
+        }
+    }
+}
 
-//             let searchArticle = deleteCanape.closest("article");       // afin de récupérer les données (canapé que l'on veut supprimer) dans l'article du dessus (dom)
-//             console.log(searchArticle);
-//             let searchId = searchArticle.dataset.id;                    // on récupère l' id
-//             console.log(searchId);
-//             let searchColor = searchArticle.dataset.color;              // on récupère la couleur
-//             console.log(searchColor);
-//             let result = cart.filter(el => el.id !== searchId || el.colors !== searchColor);
-//             console.log(result);
-//             searchArticle.remove();
-//             cartUpdateDelete(result);                                         // on supprime l'article dans le dom
-//         })
-//         function cartUpdateDelete() {   // màj du panier dans le local storage
-//             cart.push(result);
-//             let jsonCart = JSON.stringify(cart);
-//             let setCart = window.localStorage.setItem("cart", jsonCart);
-//         }
-//     }
-// }
+//! changer la quantité d'un objet dans le panier et le local storage ---> NOK
+function changeQty() {
+    let inputMoveQty = document.getElementsByClassName("itemQuantity");
+    console.log(inputMoveQty);
+    inputMoveQty.addEventListener("change", function () {
+console.log(inputMoveQty.value);
+    })
+}
+changeQty();
