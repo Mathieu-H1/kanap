02c2c8e35ea16a40c4825ce4c178a40cc08a4917
Hedbert Mathieu <Mathieu.h1@outlook.fr>
2023-04-02 12:18:56 +0200

    ajout fonction globale -> doc Ok

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,8 +1,11 @@
+function finalization() {
 const page = window.location.href;
 const url = new URL(page);
 const idNumber = url.searchParams.get("id");
 
-const searchId = document.getElementById("orderId");
+const searchId = document.getElementById("orderId");    // affichage numéro de commande
 searchId.innerHTML = idNumber;
 
-localStorage.clear();
+localStorage.clear();   // effacer le local storage
+}
+finalization();
